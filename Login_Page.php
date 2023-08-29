4704b4e8599fb9767eece5e67398012a2b0b1d2c
Shubham Sharma <133706488+SharmaShubham94@users.noreply.github.com>
2023-08-29 10:12:32 +0530

    Update Login_Page.php

--- Login_Page.php
+++ Login_Page.php
@@ -51,7 +51,7 @@
 				<span class="icon-bar"> </span>
 				<span class="icon-bar"> </span>
 			</button>
-			<a class="navbar-brand" href="#" >Optimus Infotech</a>
+			<a class="navbar-brand" href="#" >Comapny Name </a>
 		</div>
 		
 		<div class="collapse navbar-collapse" id="myNavbar">
